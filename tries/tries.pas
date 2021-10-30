1e1a718cb551cdacb75c0c6449d1c681c3ce88ef
MassiGy <ghernaoutmassi@gmail.com>
2021-10-30 16:34:41 +0200

    ./tries lookup done

--- tries/tries.pas
+++ tries/tries.pas
@@ -1,64 +1,84 @@
-program tries;
 
-const range = 26;
-const offset = 64;
-Type
-    node = ^trie;
-    trie = record
+Program tries;
+
+Const range = 26;
+
+Const offset = 64;
+
+Type 
+  node = ^trie;
+  trie = Record
     value: char;
-    pcontainer: Array[1..range] of node;
-end;
+    pcontainer: Array[1..range] Of node;
+  End;
 
-var head: node;
-var data :Pchar;
+Var head: node;
 
-procedure trieMaker(var el: node; val: char);
-var counter:integer;
+Var data : Pchar;
+
+Procedure trieMaker(Var el: node; val: char);
+
+Var counter: integer;
+Begin
+  dispose(el);
+  new(el);
+  el^.value := val;
+  For counter:=1 To range Do
+    Begin
+      el^.pcontainer[counter] := Nil;
+    End;
+
+End;
+
+
+Procedure insert(Var head: node; data: Pchar);
+
+Var index, data_length: integer;
+
+Var tmp : node;
+Begin
+  data_length := strlen(data);
+  If ( data_length= 0) Then exit;
+
+  tmp := head;
+  For index := 0 To data_length -1 Do
+    Begin
+      new(tmp^.pcontainer[ord(data[index]) - offset]);
+      tmp^.pcontainer[ord(data[index]) - offset]^.value := data[index];
+      tmp := tmp^.pcontainer[ord(data[index]) - offset];
+    End;
+End;
+
+
+function find_in_trie(head: node; query:Pchar): boolean;
+var tmp:node;
+var index, query_length:integer;
 begin
-    dispose(el);
-    new(el);
-    el^.value:=val;
-      for counter:=1 to range do
+    query_length:=strlen(query);
+    find_in_trie:=true;
+    if ( (query_length= 0) )then
     begin
-        el^.pcontainer[counter]:=nil;   
-    end;    
+        find_in_trie:=false;
+         exit;
+    end;
+    // set find_in_trie to true
+    // set tmp equal to head
+    // for each character we need to find the right slot on the current node
+    // then check if this node is equal to nil then set find_in_trie to false
+    // otherwise, set tmp to this particular node
+    // research for the new tmp node;
+    tmp:=head;
+    for index := 0 to query_length -1 do
+    begin  
+     if(tmp^.pcontainer[ord(query[index]) - offset] = nil ) then
+            begin
+                find_in_trie:=false;
+                exit;
+            end
+     else tmp := tmp^.pcontainer[ord(query[index]) - offset];
 
-end;
+    end;
 
-
-procedure insert(var head: node; data: Pchar);
-var index, data_length: integer;
-var tmp : node;
-begin
-    data_length:=strlen(data);
-    if( data_length= 0) then exit;
-    if(head=nil) then 
-        begin
-
-            // if head is null, create a new node and assign it the first char of the input
-            // then set a tmp pointer to this node to create the next node
-            // create a loop that walks through the input string.
-            // for each remaining character, take the ord and substract 65 (for uppercases) in order to get the position of this character
-            // from A (position = 1) through Z (position = 26) - ascii chart character representation maping
-            // then take the resault of this mapping formula as the correct slot in the pcontainer array of the parent node
-            // then move tmp form the parent node to the new child node
-            // repeat the process till the entire string has been inserted
-
-            // but if head can be null, that means that we always need to create a new trie.!!
-            trieMaker(head, data[0]);
-            tmp:=head;
-
-            for index := 1 to data_length -1 do
-                begin
-                    new(tmp^.pcontainer[ord(data[index]) - offset]);
-                    tmp^.pcontainer[ord(data[index]) - offset]^.value:= data[index];
-                    tmp:=tmp^.pcontainer[ord(data[index]) - offset];
-                end;
-            head:=tmp;
-            exit;
-        end;
-    
-   
 
 end;
 
@@ -79,8 +99,9 @@ end;
 
 
 
-
-begin
-    head:=nil;
-    insert(head, 'DATA')
-end.
+Begin
+  head := Nil;
+  trieMaker(head,'#');
+  insert(head, 'DATA');
+  writeln(find_in_trie(head,'D'));
+End.
